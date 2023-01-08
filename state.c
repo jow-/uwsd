jow-/uwsd

commit e4153393fe2730684dbaf2b8bec09698636dcc00

state: invoke IO read callbacks right away if buffered data available
Add facilities to query the buffered data state of connection handles and
use those to decide whether epoll registration is required on state change.

In case buffered data is found to be present and read readiness of a
descriptor required, invoke the associated IO callback right away instead
of relying on epoll notification. This is required for SSL connections
which might have pending application data stored in their context while
the underlying descriptor is fully read and not going to yield another
event.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

state.c
@@ -267,6 +267,9 @@ uwsd_state_transition(uwsd_client_context_t *cl, uwsd_connection_state_t state)
 			uloop_timeout_set(&cl->upstream.utm, timeout);
 		else
 			uloop_timeout_cancel(&cl->upstream.utm);
+
+		if (client_pending(&cl->upstream) > 0)
+			upstream_ufd_cb(&cl->upstream.ufd, ULOOP_READ);
 	}
 	else {
 		uloop_fd_delete(&cl->upstream.ufd);
@@ -280,6 +283,9 @@ uwsd_state_transition(uwsd_client_context_t *cl, uwsd_connection_state_t state)
 			uloop_timeout_set(&cl->downstream.utm, timeout);
 		else
 			uloop_timeout_cancel(&cl->downstream.utm);
+
+		if (client_pending(&cl->downstream) > 0)
+			downstream_ufd_cb(&cl->downstream.ufd, ULOOP_READ);
 	}
 	else {
 		uloop_fd_delete(&cl->downstream.ufd);

client.c
@@ -22,6 +22,7 @@
 #include <errno.h>
 
 #include <sys/time.h>
+#include <sys/ioctl.h>
 #include <sys/sendfile.h>
 #include <arpa/inet.h>
 
@@ -142,6 +143,20 @@ client_accept(uwsd_client_context_t *cl)
 		return uwsd_ssl_accept(cl);
 
 	return true;
+}
+
+__hidden ssize_t
+client_pending(uwsd_connection_t *conn)
+{
+	int pending;
+
+	if (conn->ssl)
+		return uwsd_ssl_pending(conn);
+
+	if (ioctl(conn->ufd.fd, FIONREAD, &pending) == -1)
+		return -1;
+
+	return pending;
 }
 
 __hidden ssize_t

include/client.h
@@ -97,6 +97,7 @@ __hidden void client_free(uwsd_client_context_t *, const char *, ...);
 __hidden void client_free_all(void);
 
 __hidden bool client_accept(uwsd_client_context_t *);
+__hidden ssize_t client_pending(uwsd_connection_t *);
 __hidden ssize_t client_recv(uwsd_connection_t *, void *, size_t);
 __hidden ssize_t client_send(uwsd_connection_t *, const void *, size_t);
 __hidden ssize_t client_sendv(uwsd_connection_t *, struct iovec *, size_t);

include/ssl.h
@@ -41,6 +41,7 @@ __hidden bool uwsd_ssl_init(uwsd_client_context_t *);
 __hidden void uwsd_ssl_free(uwsd_client_context_t *);
 __hidden bool uwsd_ssl_accept(uwsd_client_context_t *);
 
+__hidden ssize_t uwsd_ssl_pending(uwsd_connection_t *);
 __hidden ssize_t uwsd_ssl_recv(uwsd_connection_t *, void *, size_t);
 __hidden ssize_t uwsd_ssl_send(uwsd_connection_t *, const void *, size_t);
 __hidden ssize_t uwsd_ssl_sendv(uwsd_connection_t *, struct iovec *, size_t);

ssl.c
@@ -520,6 +520,14 @@ uwsd_ssl_accept(uwsd_client_context_t *cl)
 }
 
 __hidden ssize_t
+uwsd_ssl_pending(uwsd_connection_t *conn)
+{
+	SSL *ssl = conn->ssl;
+
+	return SSL_pending(ssl);
+}
+
+__hidden ssize_t
 uwsd_ssl_recv(uwsd_connection_t *conn, void *data, size_t len)
 {
 	SSL *ssl = conn->ssl;

commit 50127f650d5da2acaacffdde1454b5e0f91a253e

state: rework pending data handling
Do not invoke the IO callback right away when pending data is present on
a connection but schedule an immediate uloop timeout with a dedicated
callback function which invokes the IO handler in order to avoid deep
recursion when large amounts of buffered data are present.

Fixes: e415339 ("state: invoke IO read callbacks right away if buffered data available")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

state.c
@@ -188,6 +188,15 @@ upstream_utm_cb(struct uloop_timeout *utm)
 }
 
 static void
+upstream_pending_cb(struct uloop_timeout *utm)
+{
+	uwsd_client_context_t *cl = container_of(utm, uwsd_client_context_t, upstream.utm);
+
+	utm->cb = upstream_utm_cb;
+	states[cl->state].io_cb(cl, cl->state, true);
+}
+
+static void
 downstream_ufd_cb(struct uloop_fd *ufd, unsigned int events)
 {
 	uwsd_client_context_t *cl = container_of(ufd, uwsd_client_context_t, downstream.ufd);
@@ -201,6 +210,15 @@ downstream_utm_cb(struct uloop_timeout *utm)
 	uwsd_client_context_t *cl = container_of(utm, uwsd_client_context_t, downstream.utm);
 
 	states[cl->state].timeout_cb(cl, cl->state, false);
+}
+
+static void
+downstream_pending_cb(struct uloop_timeout *utm)
+{
+	uwsd_client_context_t *cl = container_of(utm, uwsd_client_context_t, downstream.utm);
+
+	utm->cb = downstream_utm_cb;
+	states[cl->state].io_cb(cl, cl->state, false);
 }
 
 static int
@@ -228,6 +246,7 @@ timeout_value(uwsd_client_context_t *cl, uwsd_timeout_kind_t tt)
 
 	return -1;
 }
+
 
 __hidden void
 uwsd_state_init(uwsd_client_context_t *cl, uwsd_connection_state_t state)
@@ -261,15 +280,19 @@ uwsd_state_transition(uwsd_client_context_t *cl, uwsd_connection_state_t state)
 	timeout = timeout_value(cl, se->timeout);
 
 	if (se->channels & CHANNEL_UPSTREAM) {
-		uloop_fd_add(&cl->upstream.ufd, events);
+		if (client_pending(&cl->upstream)) {
+			cl->upstream.utm.cb = upstream_pending_cb;
+			uloop_timeout_set(&cl->upstream.utm, 0);
+			uloop_fd_delete(&cl->upstream.ufd);
+		}
+		else {
+			uloop_fd_add(&cl->upstream.ufd, events);
 
-		if (timeout > -1)
-			uloop_timeout_set(&cl->upstream.utm, timeout);
-		else
-			uloop_timeout_cancel(&cl->upstream.utm);
-
-		if (client_pending(&cl->upstream) > 0)
-			upstream_ufd_cb(&cl->upstream.ufd, ULOOP_READ);
+			if (timeout > -1)
+				uloop_timeout_set(&cl->upstream.utm, timeout);
+			else
+				uloop_timeout_cancel(&cl->upstream.utm);
+		}
 	}
 	else {
 		uloop_fd_delete(&cl->upstream.ufd);
@@ -277,15 +300,19 @@ uwsd_state_transition(uwsd_client_context_t *cl, uwsd_connection_state_t state)
 	}
 
 	if (se->channels & CHANNEL_DOWNSTREAM) {
-		uloop_fd_add(&cl->downstream.ufd, events);
+		if (client_pending(&cl->downstream)) {
+			cl->downstream.utm.cb = downstream_pending_cb;
+			uloop_timeout_set(&cl->downstream.utm, 0);
+			uloop_fd_delete(&cl->downstream.ufd);
+		}
+		else {
+			uloop_fd_add(&cl->downstream.ufd, events);
 
-		if (timeout > -1)
-			uloop_timeout_set(&cl->downstream.utm, timeout);
-		else
-			uloop_timeout_cancel(&cl->downstream.utm);
-
-		if (client_pending(&cl->downstream) > 0)
-			downstream_ufd_cb(&cl->downstream.ufd, ULOOP_READ);
+			if (timeout > -1)
+				uloop_timeout_set(&cl->downstream.utm, timeout);
+			else
+				uloop_timeout_cancel(&cl->downstream.utm);
+		}
 	}
 	else {
 		uloop_fd_delete(&cl->downstream.ufd);