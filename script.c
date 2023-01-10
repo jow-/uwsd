jow-/uwsd

commit c95a5b65bf9de44b2219739dc57415fdd1c9cd5f

script: expose SSL cipher information
Also set a boolean `ssl: true` in the connection info dictionary to
indicate SSL enabled connections.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

script.c
@@ -44,6 +44,7 @@ static LIST_HEAD(requests);
 typedef enum {
 	UWSD_SCRIPT_DATA_PEER_ADDR,
 	UWSD_SCRIPT_DATA_LOCAL_ADDR,
+	UWSD_SCRIPT_DATA_SSL_CIPER,
 	UWSD_SCRIPT_DATA_X509_PEER_ISSUER,
 	UWSD_SCRIPT_DATA_X509_PEER_SUBJECT,
 	UWSD_SCRIPT_DATA_HTTP_VERSION,
@@ -672,7 +673,7 @@ uc_script_request_info(uc_vm_t *vm, size_t nargs)
 	const char *fields[] = {
 		"local_address", "local_port",
 		"peer_address", "peer_port",
-		"x509_peer_issuer", "x509_peer_subject"
+		"ssl", "ssl_cipher", "x509_peer_issuer", "x509_peer_subject"
 	};
 
 	if (!conn || !*conn)
@@ -899,6 +900,13 @@ handle_tlv(script_connection_t *conn, uint16_t type, uint16_t len, uint8_t *data
 			ucv_object_add(conn->req, "peer_address", ucv_string_new(addr));
 			ucv_object_add(conn->req, "peer_port", ucv_uint64_new(u16));
 		}
+
+		break;
+
+	case UWSD_SCRIPT_DATA_SSL_CIPER:
+		ucv_object_add(conn->req, "ssl", ucv_boolean_new(true));
+		ucv_object_add(conn->req, "ssl_cipher",
+			ucv_string_new_length((char *)data, len));
 
 		break;
 
@@ -1279,7 +1287,7 @@ __hidden bool
 uwsd_script_connect(uwsd_client_context_t *cl, const char *acceptkey)
 {
 	uint16_t tv[cl->http_num_headers], lv[cl->http_num_headers];
-	struct iovec iov[(8 + cl->http_num_headers) * 3];
+	struct iovec iov[(9 + cl->http_num_headers) * 3];
 	struct iovec *iop = iov;
 	ssize_t total = 0;
 	const char *s;
@@ -1298,6 +1306,11 @@ uwsd_script_connect(uwsd_client_context_t *cl, const char *acceptkey)
 	}
 
 	if (cl->listener->ssl) {
+		s = uwsd_ssl_cipher_name(&cl->downstream);
+
+		if (s)
+			total += static_tlv(&iop, UWSD_SCRIPT_DATA_SSL_CIPER, strlen(s), s);
+
 		s = uwsd_ssl_peer_issuer_name(&cl->downstream);
 
 		if (s)

include/ssl.h
@@ -47,6 +47,7 @@ __hidden ssize_t uwsd_ssl_send(uwsd_connection_t *, const void *, size_t);
 __hidden ssize_t uwsd_ssl_sendv(uwsd_connection_t *, struct iovec *, size_t);
 __hidden ssize_t uwsd_ssl_close(uwsd_connection_t *);
 
+__hidden const char *uwsd_ssl_cipher_name(uwsd_connection_t *);
 __hidden const char *uwsd_ssl_peer_subject_name(uwsd_connection_t *);
 __hidden const char *uwsd_ssl_peer_issuer_name(uwsd_connection_t *);
 

ssl.c
@@ -648,6 +648,14 @@ uwsd_ssl_close(uwsd_connection_t *conn)
 }
 
 __hidden const char *
+uwsd_ssl_cipher_name(uwsd_connection_t *conn)
+{
+	SSL *ssl = conn->ssl;
+
+	return SSL_get_cipher(ssl);
+}
+
+__hidden const char *
 uwsd_ssl_peer_subject_name(uwsd_connection_t *conn)
 {
 	SSL *ssl = conn->ssl;

commit bfafa660e58be89a8d54c209849d927778739d72

script: fix typ in enum constant name
Fixes: c95a5b6 ("script: expose SSL cipher information")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

script.c
@@ -44,7 +44,7 @@ static LIST_HEAD(requests);
 typedef enum {
 	UWSD_SCRIPT_DATA_PEER_ADDR,
 	UWSD_SCRIPT_DATA_LOCAL_ADDR,
-	UWSD_SCRIPT_DATA_SSL_CIPER,
+	UWSD_SCRIPT_DATA_SSL_CIPHER,
 	UWSD_SCRIPT_DATA_X509_PEER_ISSUER,
 	UWSD_SCRIPT_DATA_X509_PEER_SUBJECT,
 	UWSD_SCRIPT_DATA_HTTP_VERSION,
@@ -903,7 +903,7 @@ handle_tlv(script_connection_t *conn, uint16_t type, uint16_t len, uint8_t *data
 
 		break;
 
-	case UWSD_SCRIPT_DATA_SSL_CIPER:
+	case UWSD_SCRIPT_DATA_SSL_CIPHER:
 		ucv_object_add(conn->req, "ssl", ucv_boolean_new(true));
 		ucv_object_add(conn->req, "ssl_cipher",
 			ucv_string_new_length((char *)data, len));
@@ -1309,7 +1309,7 @@ uwsd_script_connect(uwsd_client_context_t *cl, const char *acceptkey)
 		s = uwsd_ssl_cipher_name(&cl->downstream);
 
 		if (s)
-			total += static_tlv(&iop, UWSD_SCRIPT_DATA_SSL_CIPER, strlen(s), s);
+			total += static_tlv(&iop, UWSD_SCRIPT_DATA_SSL_CIPHER, strlen(s), s);
 
 		s = uwsd_ssl_peer_issuer_name(&cl->downstream);
 