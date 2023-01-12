jow-/uwsd

commit ca0995d12b3d8d6c9556f39e3e2069b1820b7029

http: fix directory serving and implement directory listings
Implement proper support for serving directory trees with optional
per-directory index file lookups and fallback HTML content listings.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

file.c
@@ -18,8 +18,13 @@
 #include <string.h>
 #include <inttypes.h>
 #include <time.h>
+#include <fcntl.h>
 
 #include <sys/stat.h>
+
+#ifdef HAVE_MEMFD_CREATE
+#include <sys/mman.h>
+#endif
 
 #include "file.h"
 #include "http.h"
@@ -174,6 +179,155 @@ uwsd_file_if_unmodified_since(uwsd_client_context_t *cl, struct stat *s)
 
 		return false;
 	}
+
+	return true;
+}
+
+
+static int
+dirent_cmp(const struct dirent **a, const struct dirent **b)
+{
+	bool dir_a = !!((*a)->d_type & DT_DIR);
+	bool dir_b = !!((*b)->d_type & DT_DIR);
+
+	/* directories first */
+	if (dir_a != dir_b)
+		return dir_b - dir_a;
+
+	return alphasort(a, b);
+}
+
+static void
+print_entry(FILE *tmp, struct dirent *e,
+            const char *physpath, const char *urlpath)
+{
+	const char *type = "directory";
+	unsigned int mode = S_IROTH;
+	struct stat s;
+	char *p;
+
+	if (!strcmp(e->d_name, "."))
+		return;
+
+	p = pathexpand(e->d_name, physpath);
+
+	if (!p)
+		return;
+
+	if (!stat(p, &s)) {
+		if (S_ISDIR(s.st_mode))
+			mode |= S_IXOTH;
+		else
+			type = uwsd_file_mime_lookup(p);
+	}
+	else {
+		s.st_mode = 0;
+	}
+
+	free(p);
+
+	if ((s.st_mode & mode) != mode)
+		return;
+
+	p = htmlescape(e->d_name);
+
+	if (!p)
+		return;
+
+	fprintf(tmp,
+		"<li><strong><a href='%s/%s%s'>%s</a>%s"
+		"</strong><br /><small>modified: %s"
+		"<br />%s - %.02f kbyte<br />"
+		"<br /></small></li>\n",
+		urlpath, p, (mode & S_IXOTH) ? "/" : "",
+		p, (mode & S_IXOTH) ? "/" : "",
+		uwsd_file_unix2date(s.st_mtime),
+		type, s.st_size / 1024.0);
+
+	free(p);
+}
+
+static void
+list_entries(FILE *tmp, struct dirent **files, int count,
+             const char *physpath, const char *urlpath)
+{
+	int i;
+
+	if (!strcmp(urlpath, "/"))
+		urlpath = "";
+
+	for (i = 0; i < count; i++) {
+		print_entry(tmp, files[i], physpath, urlpath);
+		free(files[i]);
+	}
+}
+
+__hidden bool
+uwsd_file_directory_list(uwsd_client_context_t *cl, const char *physpath, const char *urlpath)
+{
+	const char *type = cl->action->data.directory.content_type;
+	char szbuf[sizeof("18446744073709551615")];
+	struct dirent **files = NULL;
+	int fd = -1, count;
+	char *title;
+	FILE *tmp;
+
+#ifdef HAVE_MEMFD_CREATE
+	if (fd == -1)
+		fd = memfd_create("uwsd-directory-listing", 0);
+#endif
+
+#ifdef HAVE_O_TMPFILE
+	if (fd == -1)
+		fd = open("/tmp", O_TMPFILE|O_RDWR);
+#endif
+
+	tmp = (fd > -1) ? fdopen(fd, "r+") : tmpfile();
+
+	if (!tmp)
+		return false;
+
+	title = htmlescape(urlpath);
+
+	if (!title)
+		return false;
+
+	fprintf(tmp,
+		"<html><head><title>Index of %1$s/</title></head>"
+		"<body><h1>Index of %1$s/</h1><hr /><ol>\n",
+		strcmp(title, "/") ? title : "");
+
+	count = scandir(physpath, &files, NULL, dirent_cmp);
+
+	list_entries(tmp, files, count, physpath, title);
+
+	free(title);
+	free(files);
+
+	fprintf(tmp, "</ol><hr /></body></html>");
+
+	cl->upstream.ufd.fd = dup(fileno(tmp));
+
+	if (cl->upstream.ufd.fd == -1) {
+		fclose(tmp);
+
+		return false;
+	}
+
+	snprintf(szbuf, sizeof(szbuf), "%lu", ftell(tmp));
+
+	fflush(tmp);
+	fclose(tmp);
+
+	lseek(cl->upstream.ufd.fd, 0, SEEK_SET);
+
+	uwsd_http_reply(cl, 200, "OK", UWSD_HTTP_REPLY_EMPTY,
+		"Content-Type", type ? type : "text/html; charset=utf-8",
+		"Content-Length", szbuf,
+		"Connection", uwsd_http_header_contains(cl, "Connection", "close") ? "close" : NULL,
+		UWSD_HTTP_REPLY_EOH);
+
+	uwsd_state_transition(cl, STATE_CONN_REPLY_SENDFILE);
 
 	return true;
 }

CMakeLists.txt
@@ -1,4 +1,5 @@
 cmake_minimum_required(VERSION 3.13)
+include(CheckCSourceCompiles)
 
 PROJECT(uwsd C)
 ADD_DEFINITIONS(-Os -Wall -Werror --std=gnu99 -ffunction-sections -fwrapv -D_GNU_SOURCE)
@@ -15,6 +16,22 @@ IF(DEBUG)
   ADD_DEFINITIONS(-DDEBUG -g3 -O0)
 ELSE()
   ADD_DEFINITIONS(-DNDEBUG)
+ENDIF()
+
+CHECK_C_SOURCE_COMPILES(
+	"#include <sys/mman.h>\nint main() { return memfd_create(\"test\", 0); }"
+	HAVE_MEMFD_CREATE)
+
+CHECK_C_SOURCE_COMPILES(
+	"#define _GNU_SOURCE\n#include <fcntl.h>\nint main() { return O_TMPFILE; }"
+	HAVE_O_TMPFILE)
+
+IF(HAVE_MEMFD_CREATE)
+	ADD_DEFINITIONS(-DHAVE_MEMFD_CREATE)
+ENDIF()
+
+IF(HAVE_O_TMPFILE)
+	ADD_DEFINITIONS(-DHAVE_O_TMPFILE)
 ENDIF()
 
 IF(USE_MBEDTLS)

config.c
@@ -242,6 +242,12 @@ static const config_block_t serve_directory_spec = {
 	.validate = validate_action,
 	.free = free_action,
 	.properties = {
+		{ "content-type", STRING,
+			offsetof(uwsd_action_t, data.directory.content_type), { 0 } },
+		{ "index-filename", STRING,
+			offsetof(uwsd_action_t, data.directory.index_filename), { 0 } },
+		{ "directory-listing", BOOLEAN,
+			offsetof(uwsd_action_t, data.directory.directory_listing), { 0 } },
 		{ 0 }
 	}
 };
@@ -809,7 +815,7 @@ parse_serve_directory(void *obj, const char *label)
 
 	action->type = UWSD_ACTION_DIRECTORY;
 
-	return check_path(label, true, &action->data.directory);
+	return check_path(label, true, &action->data.directory.path);
 }
 
 static bool
@@ -939,11 +945,14 @@ validate_action(void *obj)
 		break;
 
 	case UWSD_ACTION_DIRECTORY:
-		if (stat(action->data.directory, &s))
-			return parse_error("Failed to stat '%s': %m", action->data.directory);
+		if (stat(action->data.directory.path, &s))
+			return parse_error("Failed to stat '%s': %m", action->data.directory.path);
 
 		if (!S_ISDIR(s.st_mode))
-			return parse_error("Path '%s' exists but is not a directory", action->data.directory);
+			return parse_error("Path '%s' exists but is not a directory", action->data.directory.path);
+
+		if (strchr(action->data.directory.index_filename ?: "", '/'))
+			return parse_error("The 'index-filename' value must not contain any slashes");
 
 		break;
 
@@ -978,7 +987,7 @@ free_action(void *obj)
 
 	switch (action->type) {
 	case UWSD_ACTION_FILE:       return free(action->data.file.path);
-	case UWSD_ACTION_DIRECTORY:  return free(action->data.directory);
+	case UWSD_ACTION_DIRECTORY:  return free(action->data.directory.path);
 	case UWSD_ACTION_SCRIPT:     return uwsd_script_free(action);
 	case UWSD_ACTION_TCP_PROXY:  return free(action->data.proxy.hostname);
 	case UWSD_ACTION_UDP_PROXY:  return free(action->data.proxy.hostname);

http.c
@@ -1025,7 +1025,7 @@ static char *
 determine_path(uwsd_client_context_t *cl)
 {
 	uwsd_action_t *action = cl->action;
-	char *dst = (action->type == UWSD_ACTION_FILE) ? action->data.file.path : action->data.directory;
+	char *dst = (action->type == UWSD_ACTION_FILE) ? action->data.file.path : action->data.directory.path;
 	size_t pfx_len = strlen(cl->prefix);
 	char *url = NULL, *path = NULL, *base = NULL;
 	size_t req_len = 0;
@@ -1174,67 +1174,231 @@ http_proxy_connect(uwsd_client_context_t *cl)
 }
 
 static bool
-http_file_serve(uwsd_client_context_t *cl)
+send_file(uwsd_client_context_t *cl, const char *path, const char *type, struct stat *s)
 {
 	char szbuf[sizeof("18446744073709551615")];
+
+	if (!(s->st_mode & S_IROTH) || strrchr(path, '/')[1] == '.') {
+		errno = EACCES;
+
+		return false;
+	}
+
+	cl->upstream.ufd.fd = open(path, O_RDONLY);
+
+	if (cl->upstream.ufd.fd == -1)
+		return false;
+
+	if (uwsd_file_if_range(cl, s) &&
+	    uwsd_file_if_match(cl, s) &&
+	    uwsd_file_if_modified_since(cl, s) &&
+	    uwsd_file_if_none_match(cl, s) &&
+	    uwsd_file_if_unmodified_since(cl, s))
+	{
+		snprintf(szbuf, sizeof(szbuf), "%zu", (size_t)s->st_size);
+
+		if (!type || !*type)
+			type = uwsd_file_mime_lookup(path);
+
+		uwsd_http_reply(cl, 200, "OK", UWSD_HTTP_REPLY_EMPTY,
+			"Content-Type", type,
+			"Content-Length", szbuf,
+			"ETag", uwsd_file_mktag(s),
+			"Last-Modified", uwsd_file_unix2date(s->st_mtime),
+			"Connection", uwsd_http_header_contains(cl, "Connection", "close") ? "close" : NULL,
+			UWSD_HTTP_REPLY_EOH);
+	}
+
+	uwsd_state_transition(cl, STATE_CONN_REPLY_SENDFILE);
+
+	return true;
+}
+
+static bool
+http_file_serve(uwsd_client_context_t *cl)
+{
+	const char *type = cl->action->data.file.content_type;
 	char *path = determine_path(cl);
-	const char *mimetype = NULL;
+	bool rv = false;
 	struct stat s;
 
 	if (cl->request_method != HTTP_GET && cl->request_method != HTTP_HEAD)
 		http_error_return(cl, 405, "Method Not Allowed",
 			"The used HTTP method is invalid for the requested resource\n");
 
-	if (path && stat(path, &s) == 0)
-		cl->upstream.ufd.fd = open(path, O_RDONLY);
-	else
-		cl->upstream.ufd.fd = -1;
-
-	if (cl->upstream.ufd.fd == -1) {
-		free(path);
-
-		switch (errno) {
-		case EACCES:
-			http_error_return(cl, 403, "Permission Denied",
-				"Access to the requested file is forbidden\n");
-
-		case ENOENT:
-			http_error_return(cl, 404, "Not Found",
-				"The requested path does not exist on this server\n");
-
-		default:
-			http_error_return(cl, 500, "Internal Server Error",
-				"Unable to serve requested file: %s\n",
-				strerror(errno));
-		}
-	}
-
-	if (uwsd_file_if_range(cl, &s) &&
-	    uwsd_file_if_match(cl, &s) &&
-	    uwsd_file_if_modified_since(cl, &s) &&
-	    uwsd_file_if_none_match(cl, &s) &&
-	    uwsd_file_if_unmodified_since(cl, &s))
-	{
-		snprintf(szbuf, sizeof(szbuf), "%zu", (size_t)s.st_size);
-
-		if (cl->action->type == UWSD_ACTION_FILE)
-			mimetype = cl->action->data.file.content_type;
-
-		if (!mimetype || !*mimetype)
-			mimetype = uwsd_file_mime_lookup(path);
-
-		uwsd_http_reply(cl, 200, "OK", UWSD_HTTP_REPLY_EMPTY,
-			"Content-Type", mimetype,
-			"Content-Length", szbuf,
-			"ETag", uwsd_file_mktag(&s),
-			"Last-Modified", uwsd_file_unix2date(s.st_mtime),
-			"Connection", uwsd_http_header_contains(cl, "Connection", "close") ? "close" : NULL,
-			UWSD_HTTP_REPLY_EOH);
-	}
+	rv = (path && !stat(path, &s) && send_file(cl, path, type, &s));
 
 	free(path);
 
-	uwsd_state_transition(cl, STATE_CONN_REPLY_SENDFILE);
+	switch (rv ? 0 : errno) {
+	case 0:
+		return true;
+
+	case EACCES:
+		http_error_return(cl, 403, "Permission Denied",
+			"Access to the requested file is forbidden\n");
+		break;
+
+	case ENOENT:
+		http_error_return(cl, 404, "Not Found",
+			"The requested path does not exist on this server\n");
+		break;
+
+	default:
+		http_error_return(cl, 500, "Internal Server Error",
+			"Unable to serve requested file: %m\n");
+		break;
+	}
+}
+
+static char *
+find_index_file(uwsd_client_context_t *cl, const char *path, struct stat *s)
+{
+	char *candidates, *p, *indexfile = NULL;
+
+	if (cl->action->data.directory.index_filename)
+		candidates = strdup(cl->action->data.directory.index_filename);
+	else
+		candidates = strdup("index.html;index.htm;default.html;default.htm");
+
+	if (!candidates) {
+		errno = ENOMEM;
+
+		return NULL;
+	}
+
+	for (p = strtok(candidates, ",; \t\n"); p; p = strtok(NULL, ",; \t\n")) {
+		indexfile = pathexpand(p, path);
+
+		if (!indexfile)
+			return NULL;
+
+		if (stat(indexfile, s) == -1) {
+			uwsd_http_debug(cl, "Unable to stat() index file candiate '%s': %m", indexfile);
+			goto skip;
+		}
+
+		if (!S_ISREG(s->st_mode)) {
+			uwsd_http_debug(cl, "Index file candiate '%s' is not a regular file", indexfile);
+			goto skip;
+		}
+
+		if (!(s->st_mode & S_IROTH)) {
+			uwsd_http_debug(cl, "Index file candiate '%s' is not world readable", indexfile);
+			goto skip;
+		}
+
+		break;
+
+skip:
+		free(indexfile);
+		indexfile = NULL;
+	}
+
+	free(candidates);
+
+	errno = 0;
+
+	return indexfile;
+}
+
+static bool
+http_directory_serve(uwsd_client_context_t *cl)
+{
+	const char *type = cl->action->data.directory.content_type;
+	char *path = NULL, *base = NULL, *url = NULL, *p;
+	bool rv = false;
+	struct stat s;
+
+	if (cl->request_method != HTTP_GET && cl->request_method != HTTP_HEAD)
+		http_error_return(cl, 405, "Method Not Allowed",
+			"The used HTTP method is invalid for the requested resource\n");
+
+	base = pathexpand(cl->action->data.directory.path, NULL);
+	url = pathclean(urldecode(cl->request_uri), -1);
+
+	if (!base || !url)
+		goto error500;
+
+	if (*url != '/')
+		goto error404;
+
+	url[strcspn(url, "?")] = 0;
+	path = pathexpand(url + strspn(url, "/"), base);
+
+	if (!path)
+		goto error500;
+
+	if (!pathmatch(base, path))
+		goto error403;
+
+	if (stat(path, &s) == -1) {
+		switch (errno) {
+		case EACCES: goto error403;
+		default:     goto error404;
+		}
+	}
+
+	if (S_ISDIR(s.st_mode)) {
+		if (!(s.st_mode & S_IXOTH))
+			goto error403;
+
+		p = find_index_file(cl, path, &s);
+
+		if (!p) {
+			if (errno)
+				goto error500;
+
+			if (!cl->action->data.directory.directory_listing)
+				goto error403;
+
+			rv = uwsd_file_directory_list(cl, path, url);
+		}
+		else {
+			rv = send_file(cl, p, type, &s);
+		}
+
+		free(p);
+	}
+	else {
+		rv = send_file(cl, path, NULL, &s);
+	}
+
+	switch (rv ? 0 : errno) {
+	case 0:      goto success;
+	case EACCES: goto error403;
+	case ENOENT: goto error404;
+	default:     goto error500;
+	}
+
+error403:
+	free(base);
+	free(path);
+	free(url);
+
+	http_error_return(cl, 403, "Permission Denied",
+		"Access to the requested path is forbidden");
+
+error404:
+	free(base);
+	free(path);
+	free(url);
+
+	http_error_return(cl, 404, "Not Found",
+		"The requested path does not exist on this server");
+
+error500:
+	free(base);
+	free(path);
+	free(url);
+
+	http_error_return(cl, 500, "Internal Server Error",
+		"Unable to serve requested path: %m\n");
+
+success:
+	free(url);
+	free(path);
+	free(base);
 
 	return true;
 }
@@ -1341,9 +1505,12 @@ uwsd_http_state_request_header(uwsd_client_context_t *cl, uwsd_connection_state_
 				if (!http_proxy_connect(cl))
 					return;
 			}
-			else if (cl->action->type == UWSD_ACTION_FILE ||
-			         cl->action->type == UWSD_ACTION_DIRECTORY) {
+			else if (cl->action->type == UWSD_ACTION_FILE) {
 				if (!http_file_serve(cl))
+					return;
+			}
+			else if (cl->action->type == UWSD_ACTION_DIRECTORY) {
+				if (!http_directory_serve(cl))
 					return;
 			}
 			else if (cl->action->type == UWSD_ACTION_SCRIPT) {
@@ -1477,6 +1644,9 @@ uwsd_http_state_response_sendfile(uwsd_client_context_t *cl, uwsd_connection_sta
 	}
 
 	if (wlen == 0) {
+		close(cl->upstream.ufd.fd);
+		cl->upstream.ufd.fd = -1;
+
 		/* Close connection? */
 		if (cl->http_version < 0x0101 || uwsd_http_header_contains(cl, "Connection", "close"))
 			return client_free(cl, "closing connection");

include/file.h
@@ -18,6 +18,8 @@
 #define UWSD_FILE_H
 
 #include <stdbool.h>
+#include <unistd.h>
+#include <dirent.h>
 
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -35,5 +37,7 @@ __hidden bool uwsd_file_if_modified_since(uwsd_client_context_t *, struct stat *
 __hidden bool uwsd_file_if_none_match(uwsd_client_context_t *, struct stat *);
 __hidden bool uwsd_file_if_range(uwsd_client_context_t *, struct stat *);
 __hidden bool uwsd_file_if_unmodified_since(uwsd_client_context_t *, struct stat *);
+
+__hidden bool uwsd_file_directory_list(uwsd_client_context_t *, const char *, const char *);
 
 #endif /* UWSD_FILE_H */

include/listen.h
@@ -52,7 +52,13 @@ typedef struct uwsd_action {
 			char *path;
 			char *content_type;
 		} file;
-		char *directory;
+		struct
+		{
+			char *path;
+			char *content_type;
+			char *index_filename;
+			bool directory_listing;
+		} directory;
 		struct {
 			struct uloop_timeout timeout;
 			struct uloop_process proc;

commit 76f9341ed81ce97d28660fff9ecaf2bc030630c6

file: pass permissions to `open()` with `O_TMPFILE`
Solves the following compilation error on modern systems:

    open with O_CREAT or O_TMPFILE in second argument needs 3 arguments

Fixes: ca0995d ("http: fix directory serving and implement directory listings")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

file.c
@@ -279,7 +279,7 @@ uwsd_file_directory_list(uwsd_client_context_t *cl, const char *physpath, const 
 
 #ifdef HAVE_O_TMPFILE
 	if (fd == -1)
-		fd = open("/tmp", O_TMPFILE|O_RDWR);
+		fd = open("/tmp", O_TMPFILE|O_RDWR, 0600);
 #endif
 
 	tmp = (fd > -1) ? fdopen(fd, "r+") : tmpfile();