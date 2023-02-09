jow-/uwsd

commit d6aa4ec1d4a7b6159a0889c81051b778bc27072c

config: use common string pool for config values
Use a global string pointer list to track string values assigned to the
various configuration structures in order to support cleanly freeing
nested union objects with different types.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

config.c
@@ -118,6 +118,8 @@ static void free_auth(void *obj);
 static bool validate_ssl(void *obj);
 static void free_ssl(void *obj);
 
+static void free_toplevel(void *obj);
+
 static const config_block_t ssl_spec = {
 	.size = sizeof(uwsd_ssl_t),
 	.validate = validate_ssl,
@@ -394,6 +396,7 @@ static const config_block_t listen_spec = {
 
 static const config_block_t toplevel_spec = {
 	.size = sizeof(uwsd_config_t),
+	.free = free_toplevel,
 	.properties = {
 		{ "default-charset", STRING,
 			offsetof(uwsd_config_t, default_charset), { 0 } },
@@ -407,6 +410,15 @@ static const config_block_t toplevel_spec = {
 	}
 };
 
+
+static char *
+addstr(const char *s)
+{
+	config->strings = xrealloc(config->strings, sizeof(char *) * (config->nstrings + 1));
+	config->strings[config->nstrings] = xstrdup(s);
+
+	return config->strings[config->nstrings++];
+}
 
 static char
 hex(char c)
@@ -564,10 +576,6 @@ config_free_object(const config_block_t *spec, void *base)
 
 	for (prop = spec->properties; prop->name; prop++) {
 		switch (prop->type) {
-		case STRING:
-			free(char_ptr(prop, base));
-			break;
-
 		case NESTED_MULTIPLE:
 			list_for_each_safe(e, tmp, list_ptr(prop, base)) {
 				list_del(e);
@@ -616,7 +624,7 @@ config_parse_value(const char **input, const config_prop_t *prop, void *base)
 		if (!p || !*p)
 			return parse_error("Expecting non-empty value");
 
-		char_ptr(prop, base) = strdup(p);
+		char_ptr(prop, base) = addstr(p);
 
 		break;
 
@@ -694,7 +702,7 @@ config_parse_value(const char **input, const config_prop_t *prop, void *base)
 
 			if (*e) {
 				l = xrealloc(l, sizeof(char *) * (n + 2));
-				l[n++] = xstrdup(e);
+				l[n++] = addstr(e);
 				l[n] = NULL;
 			}
 
@@ -837,7 +845,7 @@ parse_hostname_match(void *obj, const char *label)
 		return parse_error("Expecting hostname value for 'match-hostname' property");
 
 	match->type = UWSD_MATCH_HOSTNAME;
-	match->data.value = xstrdup(label);
+	match->data.value = addstr(label);
 
 	return true;
 }
@@ -851,7 +859,7 @@ parse_path_match(void *obj, const char *label)
 		return parse_error("Expecting absolute path value for 'match-path' property");
 
 	match->type = UWSD_MATCH_PATH;
-	match->data.value = xstrdup(label);
+	match->data.value = addstr(label);
 
 	return true;
 }
@@ -1114,7 +1122,7 @@ parse_auth_basic(void *obj, const char *label)
 	uwsd_auth_t *auth = obj;
 
 	auth->type = UWSD_AUTH_BASIC;
-	auth->data.basic.realm = xstrdup(label ? label : "Protected area");
+	auth->data.basic.realm = addstr(label ? label : "Protected area");
 
 	return true;
 }
@@ -1181,7 +1189,7 @@ parse_backend(void *obj, const char *label)
 	if (!label || !*label)
 		return parse_error("Expecting name for 'backend' property");
 
-	backend->name = xstrdup(label);
+	backend->name = addstr(label);
 
 	return true;
 }
@@ -1280,6 +1288,18 @@ free_ssl(void *obj)
 	uwsd_ssl_t *ssl = obj;
 
 	return uwsd_ssl_ctx_free(ssl);
+}
+
+
+static void
+free_toplevel(void *obj)
+{
+	uwsd_config_t *conf = obj;
+
+	while (conf->nstrings)
+		free(conf->strings[--conf->nstrings]);
+
+	free(conf->strings);
 }
 
 

include/config.h
@@ -35,6 +35,8 @@ typedef struct {
 	char **mimetypes;
 	struct list_head backends;
 	struct list_head listeners;
+	char **strings;
+	size_t nstrings;
 } uwsd_config_t;
 
 extern uwsd_config_t *config;

commit 3b746ec2d4711894cad22d278b74dd0658f7d4a8

config: fix a number of double free instances
Since the introduction of a common string pool for the configuration values
we must not free those string values manually anymore to avoid a later double
free on unloading the configuration context.

Fixes: d6aa4ec ("config: use common string pool for config values")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

config.c
@@ -89,7 +89,6 @@ property_ptr(const config_prop_t *prop, void *base)
 
 static bool parse_backend(void *obj, const char *label);
 static bool validate_backend(void *obj);
-static void free_backend(void *obj);
 
 static bool parse_listen(void *obj, const char *label);
 static bool validate_listen(void *obj);
@@ -98,7 +97,6 @@ static void free_listen(void *obj);
 static bool parse_protocol_match(void *obj, const char *label);
 static bool parse_hostname_match(void *obj, const char *label);
 static bool parse_path_match(void *obj, const char *label);
-static void free_match(void *obj);
 
 static bool parse_serve_file(void *obj, const char *label);
 static bool parse_serve_directory(void *obj, const char *label);
@@ -113,7 +111,6 @@ static void free_action(void *obj);
 static bool parse_auth_basic(void *obj, const char *label);
 static bool parse_auth_mtls(void *obj, const char *label);
 static bool validate_auth(void *obj);
-static void free_auth(void *obj);
 
 static bool validate_ssl(void *obj);
 static void free_ssl(void *obj);
@@ -212,7 +209,6 @@ static const config_block_t backend_spec = {
 	.size = sizeof(uwsd_backend_t),
 	.init = parse_backend,
 	.validate = validate_backend,
-	.free = free_backend,
 	.properties = {
 		ACTION_PROPERTIES(uwsd_backend_t),
 		{ 0 }
@@ -222,7 +218,6 @@ static const config_block_t backend_spec = {
 static const config_block_t match_protocol_spec = {
 	.size = sizeof(uwsd_match_t),
 	.init = parse_protocol_match,
-	.free = free_match,
 	.properties = {
 		MATCH_PROPERTIES(uwsd_match_t),
 		ACTION_PROPERTIES(uwsd_match_t),
@@ -233,7 +228,6 @@ static const config_block_t match_protocol_spec = {
 static const config_block_t match_hostname_spec = {
 	.size = sizeof(uwsd_match_t),
 	.init = parse_hostname_match,
-	.free = free_match,
 	.properties = {
 		MATCH_PROPERTIES(uwsd_match_t),
 		ACTION_PROPERTIES(uwsd_match_t),
@@ -244,7 +238,6 @@ static const config_block_t match_hostname_spec = {
 static const config_block_t match_path_spec = {
 	.size = sizeof(uwsd_match_t),
 	.init = parse_path_match,
-	.free = free_match,
 	.properties = {
 		MATCH_PROPERTIES(uwsd_match_t),
 		ACTION_PROPERTIES(uwsd_match_t),
@@ -372,7 +365,6 @@ static const config_block_t auth_basic_spec = {
 	.size = sizeof(uwsd_auth_t),
 	.init = parse_auth_basic,
 	.validate = validate_auth,
-	.free = free_auth,
 	.properties = {
 		{ "username", STRING,
 			offsetof(uwsd_auth_t, data.basic.username), { 0 } },
@@ -388,7 +380,6 @@ static const config_block_t auth_mtls_spec = {
 	.size = sizeof(uwsd_auth_t),
 	.init = parse_auth_mtls,
 	.validate = validate_auth,
-	.free = free_auth,
 	.properties = {
 		{ "require-issuer", STRING,
 			offsetof(uwsd_auth_t, data.mtls.require_issuer), { 0 } },
@@ -1129,18 +1120,6 @@ free_action(void *obj)
 	}
 }
 
-static void
-free_match(void *obj)
-{
-	uwsd_match_t *match = obj;
-
-	switch (match->type) {
-	case UWSD_MATCH_PATH:     return free(match->data.value);
-	case UWSD_MATCH_HOSTNAME: return free(match->data.value);
-	default:                  break;
-	}
-}
-
 
 static bool
 parse_auth_basic(void *obj, const char *label)
@@ -1191,21 +1170,6 @@ validate_auth(void *obj)
 	return true;
 }
 
-static void
-free_auth(void *obj)
-{
-	uwsd_auth_t *auth = obj;
-
-	switch (auth->type) {
-	case UWSD_AUTH_BASIC:
-		free(auth->data.basic.realm);
-		break;
-
-	case UWSD_AUTH_MTLS:
-		break;
-	}
-}
-
 
 static bool
 parse_backend(void *obj, const char *label)
@@ -1237,14 +1201,6 @@ validate_backend(void *obj)
 			return parse_error("Name '%s' already used by another backend directive", backend->name);
 
 	return true;
-}
-
-static void
-free_backend(void *obj)
-{
-	uwsd_backend_t *backend = obj;
-
-	free(backend->name);
 }
 
 