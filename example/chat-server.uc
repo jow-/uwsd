jow-/uwsd

commit 01ed5275b437121c1b498421fae5fdf39c74b548

script: move server api functions into separate object
Move the uwsd provided script API functions into a dedicated global "uwsd"
dictionary in order to avoid polluting the global stdlib namespace.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

example/chat-server.uc
@@ -22,7 +22,7 @@ function unicast(conn, msg) {
 }
 
 function broadcast(msg) {
-	for (let client in connections())
+	for (let client in uwsd.connections())
 		unicast(client, msg);
 }
 
@@ -128,7 +128,7 @@ export function onData(connection, msg)
 		}
 
 		// Check for nickname uniqueness
-		for (let conn in connections()) {
+		for (let conn in uwsd.connections()) {
 			if (conn != connection) {
 				if (conn.data()?.nickname == msg.value) {
 					return unicast(connection, {
@@ -167,7 +167,7 @@ export function onData(connection, msg)
 		// Send back to requesting client
 		unicast(connection, {
 			type: 'client-list',
-			clients: map(connections(), cctx => cctx.data())
+			clients: map(uwsd.connections(), cctx => cctx.data())
 		});
 		break;
 

example/cgi-wrapper.uc
@@ -144,7 +144,7 @@ export function onRequest(request, method, uri)
 		cgi_env[`HTTP_${uc(replace(hdrname, /\W+/g, '_'))}`] = hdrvalue;
 
 	// Spawn script process
-	let proc = spawn(script.path, [ script.path ], cgi_env);
+	let proc = uwsd.spawn(script.path, [ script.path ], cgi_env);
 
 	// Store process handle in the request context
 	request.data(proc);

script.c
@@ -1377,12 +1377,6 @@ uc_script_spawn(uc_vm_t *vm, size_t nargs)
 	}
 }
 
-static const uc_function_list_t global_fns[] = {
-	{ "connections", uc_script_connections },
-	{ "sha1digest",  uc_script_sha1digest },
-	{ "spawn",       uc_script_spawn },
-};
-
 
 /* -- Scripting host implementation -------------------- */
 
@@ -1746,6 +1740,21 @@ handle_stderr(struct uloop_fd *ufd, unsigned int events)
 	handle_stdio(action, ufd->fd, UWSD_PRIO_WARN);
 }
 
+static void
+uc_script_api_init(uc_vm_t *vm)
+{
+	uc_value_t *api;
+
+	/* build uwsd api table */
+	api = ucv_object_new(vm);
+
+	ucv_object_add(api, "connections", ucv_cfunction_new("connections", uc_script_connections));
+	ucv_object_add(api, "sha1digest", ucv_cfunction_new("sha1digest", uc_script_sha1digest));
+	ucv_object_add(api, "spawn", ucv_cfunction_new("spawn", uc_script_spawn));
+
+	ucv_object_add(uc_vm_scope_get(vm), "uwsd", api);
+}
+
 static int
 script_context_run(const char *sockpath, const char *scriptpath)
 {
@@ -1776,7 +1785,7 @@ script_context_run(const char *sockpath, const char *scriptpath)
 	uc_vm_exception_handler_set(&ctx.vm, handle_exception);
 	uc_stdlib_load(uc_vm_scope_get(&ctx.vm));
 
-	uc_function_list_register(uc_vm_scope_get(&ctx.vm), global_fns);
+	uc_script_api_init(&ctx.vm);
 
 	rc = uc_vm_execute(&ctx.vm, prog, &result);
 

commit 6d2cc9690de46cf197a7a48c5d3ba13b53a4025e

example: fix null access in chat server example
Fixes: 01ed527 ("script: move server api functions into separate object")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

example/chat-server.uc
@@ -32,7 +32,7 @@ function broadcast_status() {
 
 	broadcast({
 		type: 'server-status',
-		msg: `The time is now ${ts}, there are ${length(connections())} clients connected`
+		msg: `The time is now ${ts}, there are ${length(uwsd.connections())} clients connected`
 	});
 
 	timer(status_interval, broadcast_status);